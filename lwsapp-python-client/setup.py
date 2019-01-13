b6df7a2e8be898a1d8bc40c3cb4db1a032430253
vicziani <viczian.istvan@gmail.com>
2019-01-13 22:09:35 +0100

    Python client

---
+++ lwsapp-python-client/setup.py
@@ -0,0 +1,10 @@
+#env/bin python
+
+# https://caremad.io/posts/2013/07/setup-vs-requirement/
+
+from distutils.core import setup
+
+setup(name='pwapp-python-client',
+      version='1.0',
+      packages=[],
+     )
