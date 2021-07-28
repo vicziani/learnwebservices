1810d3864ef35329924bae2ca09ba1102b236537
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2021-07-28 18:20:56 -0300

    Removing unused file

--- lwsapp-php-client/hello-client.py
+++
@@ -1,7 +0,0 @@
-import zeep
-
-wsdl = 'http://www.learnwebservices.com/services/hello?wsdl'
-client = zeep.Client(wsdl=wsdl)
-request = {'Name': 'John Doe'}
-response = client.service.SayHello(request)
-print(response)
